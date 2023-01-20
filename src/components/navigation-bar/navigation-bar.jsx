446158a019316c88711e11f708397358ea3102f5
Ksenia <108214826+Zhikiki@users.noreply.github.com>
2023-01-20 17:56:00 +0100

    Created NavigationBar component

---
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -0,0 +1,37 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export const NavigationBar = ({ user, onLoggedOut }) => {
+  return (
+    <Navbar collapseOnSelect expand='md' bg='light' variant='light'>
+      <Container>
+        <Navbar.Brand as={Link} to='/'>
+          MyFlix
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='me-auto'>
+            {!user && (
+              <>
+                <Nav.Link as={Link} to='/login'>
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to='/signup'>
+                  Signup
+                </Nav.Link>
+              </>
+            )}
+            {user && (
+              <>
+                <Nav.Link as={Link} to='/'>
+                  Home
+                </Nav.Link>
+                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
